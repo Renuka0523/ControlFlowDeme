c582c5eee79723cb56d44a763f2a09cfcc614f46
Renuka Ajay Chougule <renukachougule.0523@gmail.com>
2024-11-28 23:53:30 +0530

    Add Changes Into ControlFlowDemo

--- ControlFlowDeme/Program.cs
+++ ControlFlowDeme/Program.cs
@@ -1,12 +1,14 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace ControlFlowDeme
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            int Hour = 10;
+            //  * If Else Statement *
+            /*int Hour = 10;
 
             if (Hour > 0 && Hour < 12) 
             {
@@ -20,24 +22,56 @@ namespace ControlFlowDeme
             else
             {
                 Console.WriteLine("It's Evenning");
-            }
-
-                 /* int Age = 20;
-
-            if (Age > 0 && Age < 10)
-            {
-                Console.WriteLine("He is Child ");
-            }
-            else if (Age >= 10 && Age < 25)
-            {
-                Console.WriteLine("He is Mature");
-            }
-
-            else 
-            {
-                Console.WriteLine("He is Elder");
-            
             }*/
+
+            /* int Age = 20;
+
+         if (Age > 0 && Age < 10)
+         {
+           Console.WriteLine("He is Child ");
+         }
+             else if (Age >= 10 && Age < 25)
+            {
+           Console.WriteLine("He is Mature");
+             }
+
+             else 
+                 {
+           Console.WriteLine("He is Elder");
+
+             }*/
+
+
+            /*  bool IsGoldCustomer = true;
+
+              float Price = (IsGoldCustomer) ? 19.95f : 29.90f;
+              {
+                  Console.WriteLine(Price);
+              } */
+
+            // * Switch / Case Statement *
+
+            var season = Season.Sammer;
+
+            switch (season) 
+            {
+                case Season.Sammer :
+
+                    Console.WriteLine("Its Perfect go to beach");
+                    break;
+                case Season.Rainy :
+                    Console.WriteLine("Its Beautiful season");
+                    break ;
+
+
+                default:
+                    Console.WriteLine("Its defficult to find season");
+                    break;
+            
+            }
+
+
+
             
         }
     }
